875bf04d87761ac6726f3227df754d55fa498222
sumastrae <suamstrae@163.com>
2019-04-11 23:00:41 +0800

    Drop Dropout layer in helloworld

--- helloworld.py
+++ helloworld.py
@@ -10,7 +10,7 @@ model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(28, 28)),
   tf.keras.layers.Dense(512, activation=tf.nn.relu), # 全连接层
   tf.keras.layers.BatchNormalization(),              # 标准化层
-  tf.keras.layers.Dropout(0.2),                      # dropout层，drop rate = 0.2
+  #tf.keras.layers.Dropout(0.2),                      # dropout层，drop rate = 0.2
   tf.keras.layers.Dense(10, activation=tf.nn.softmax)# 全连接层
 ])
 model.compile(optimizer='adam',                      # 优化器使用Adam
