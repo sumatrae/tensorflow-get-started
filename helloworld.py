efeb24232e704254daf6c6eee8e7958e69f060b4
ezjinfe <felix.f.jin@ericsson.com>
2019-04-11 18:13:56 +0800

    Add fashion mnist demo and imdb demo

--- helloworld.py
+++ helloworld.py
@@ -10,12 +10,12 @@ model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(28, 28)),
   tf.keras.layers.Dense(512, activation=tf.nn.relu), # 全连接层
   tf.keras.layers.BatchNormalization(),              # 标准化层
-  tf.keras.layers.Dropout(0.2),                      # dropout层，drop rate = 0.2
+  tf.keras.layers.Dropout(0.1),                      # dropout层，drop rate = 0.2
   tf.keras.layers.Dense(10, activation=tf.nn.softmax)# 全连接层
 ])
 model.compile(optimizer='adam',                      # 优化器使用Adam
               loss='sparse_categorical_crossentropy',# 损失函数
               metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=20)
+model.fit(x_train, y_train, epochs=5)
 model.evaluate(x_test, y_test)
